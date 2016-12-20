605862d83ed4b69dde6284201bfccbe0d7af7e84
Ricky Miller <ricky.miller@gmail.com>
2016-12-19 23:06:04 -0500

    Add clearPicker and createLink

--- index.js
+++ index.js
@@ -2,23 +2,21 @@
   let matches
   const input = document.querySelector('.input')
   const picker = document.querySelector('.picker')
+  const clearPicker = () => picker.innerHTML = ''
+  const updatePicker = (str, matches) => clearPicker() & matches.forEach((match) => picker.appendChild(createLink(str, match)))
+
+  const createLink = (str, match) => {
+    const link = document.createElement('a')
+    link.innerHTML = match.emoji
+    link.setAttribute('title', match.name)
+    link.setAttribute('class', 'emoji')
+    link.addEventListener('click', () => updateText(str, match.emoji))
+    return link
+  }
 
   const updateText = (last, emoji) => {
     input.value = input.value.replace(last, emoji)
-    input.focus()
-    picker.innerHTML = ''
-  }
-
-  const updatePicker = (str, matches) => {
-    picker.innerHTML = ''
-    matches.forEach((match) => {
-      const a = document.createElement('a')
-      a.innerHTML = match.emoji
-      a.setAttribute('title', match.name)
-      a.setAttribute('class', 'emoji')
-      a.addEventListener('click', () => updateText(str, match.emoji))
-      picker.appendChild(a)
-    })
+    input.focus() & clearPicker()
   }
 
   const find = (str) => {
@@ -32,7 +30,7 @@
     const lastWord = val.substring(val.lastIndexOf(' ') + 1, val.length)
     event.keyCode === 13 && matches.length && updateText(lastWord, matches[0].emoji)
     const match = /:[a-z]/
-    lastWord.match(match) ? find(lastWord) : picker.innerHTML = ''
+    lastWord.match(match) ? find(lastWord) : clearPicker()
   }
 
   input.addEventListener('keyup', onKeyUp)
