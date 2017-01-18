fc3b347450cf4cd76b42a60c6464ec1c359c8199
Ricky Miller <ricky.miller@gmail.com>
2017-01-18 02:06:54 -0500

    Load emoji from JSON

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 (() => {
+  let emoji
   let matches
+
+  const getJSON = (url, successHandler, errorHandler) => {
+    const xhr = new window.XMLHttpRequest()
+    xhr.open('get', url, true)
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        const status = xhr.status
+        if (status === 200) {
+          successHandler && successHandler(JSON.parse(xhr.responseText))
+        } else {
+          errorHandler && errorHandler(status)
+        }
+      }
+    }
+    xhr.send()
+  }
+
   const input = document.querySelector('.input')
   const picker = document.querySelector('.picker')
   const clearPicker = () => { picker.innerHTML = '' }
-  const updatePicker = (str, matches) => clearPicker() & matches.forEach((match) => picker.appendChild(createLink(str, match)))
+  const updatePicker = (str, matches) => {
+    clearPicker() & matches.forEach((match) => {
+      picker.appendChild(createLink(str, match))
+    })
+  }
 
   const createLink = (str, match) => {
     const link = document.createElement('a')
@@ -21,17 +43,27 @@
 
   const find = (str) => {
     const match = new RegExp(`^${str.substring(1, str.length)}`)
-    matches = Object.keys(window.emoji).filter((emoj) => match.test(emoj)).map((key) => ({ name: key, emoji: window.emoji[key] }))
-    matches.length && updatePicker(str, matches)
+    matches = Object.keys(emoji).filter((emoj) => {
+      return match.test(emoj)
+    }).map((key) => {
+      return { name: key, emoji: emoji[key] }
+    })
+    return matches.length && updatePicker(str, matches)
   }
 
   const onInput = (event) => {
     const val = input.value
     const lastWord = val.substring(val.lastIndexOf(' ') + 1, val.length)
     const match = /:[a-z0-9]/
-    event.keyCode === 13 && matches.length && lastWord.match(match) && updateText(lastWord, matches[0].emoji)
-    lastWord.match(match) ? find(lastWord) : clearPicker()
+    if (event.keyCode === 13 && matches.length && lastWord.match(match)) {
+      return updateText(lastWord, matches[0].emoji)
+    }
+    return lastWord.match(match) ? find(lastWord) : clearPicker()
   }
 
-  ['input', 'keyup'].map((event) => input.addEventListener(event, onInput))
+  getJSON('emoji.json', (data) => {
+    emoji = data
+    const events = ['input', 'keyup']
+    events.map((event) => input.addEventListener(event, onInput))
+  })
 })()
